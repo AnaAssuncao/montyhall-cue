7806f260f68efe2228922f0776dcd07c067c48a8
AnaAssuncao <apoassuncao@gmail.com>
2022-12-26 17:49:40 -0300

    feat: montyhall

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="doors">
+    <div v-for="door in amountDoor" :key="door" @click="handleSelectDoor(door)">
+     <Door 
+        :isSelect='door == select' 
+        :hasGift='chosenDoor == door' 
+        :isOpen='isOpen.includes(door)' 
+        :doorNumber="door"></Door>
+    </div>
+  </div>
+
+  <div v-if='isOpen.length<3'>
+      <button class='door-btn' @click="handleOpenDoor()">Abrir uma porta</button>
+    </div>
+    <div v-if='isOpen.length===3'>
+      <button class='door-btn' @click="handleResponse()">Porta com presente</button>
+    </div>
+    <div>
+      <button class='door-btn' @click="handleRestart()">Reiniciar</button>
+    </div>
+    <div>
+      <p>{{ msgDoor }}</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Door from "./components/Door"
 
+const chosenDoor = Math.floor(Math.random() * 5 + 1)
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Door
+  },
+  data(){
+    return{
+      amountDoor:5,
+      chosenDoor: chosenDoor,
+      select: 0,
+      isOpen:[],
+      msgDoor:''
+    }
+  },
+  methods: {
+    handleSelectDoor(number){
+      this.select=number
+    },
+    handleOpenDoor(){
+      if(this.select===0){
+        alert("Selecione uma porta");
+        return
+      }
+
+      const number = Math.floor(Math.random() * 5 + 1)
+      if(number!==this.chosenDoor && this.select!==number && !this.isOpen.includes(number)){
+        this.isOpen.push(number)
+        this.msgDoor = 'Quer mudar a porta selecionada?'
+        return
+      }
+
+      this.handleOpenDoor()
+    },
+    handleResponse(){
+        this.isOpen.push(this.chosenDoor)
+        this.msgDoor = this.select === this.chosenDoor ? "Parabéns, você acertou!": "Não foi dessa vez, tente novamente!"
+    },
+    handleRestart(){
+      this.isOpen=[]
+      this.select=0
+      this.msgDoor=''
+      const chosenDoor = Math.floor(Math.random() * 5 + 1)
+      this.chosenDoor=chosenDoor
+    },
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.doors{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+.door-btn{
+  background-color: crimson;
+  border-radius:20px;
+  cursor: pointer;
+  font-size: 14px;
+  margin:24px;
+  padding:16px;
+}
+
 </style>
---
+++ src/components/Door.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="door-area">
+    <div class="door-frame" :class="{selected : !isOpen && isSelect}">
+      <div class="door-gift" >
+        <Gift v-if="hasGift" />
+      </div>
+    </div>
+    <div class="door" v-show="!isOpen" >
+      <div class="door-number" :class="{'selected-number' : isSelect}">{{doorNumber}}</div>
+      <div class="door-handle"  :class="{selected : isSelect}"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Gift from './Gift'
+
+export default {
+  name: 'Door-montyhall',
+  components: {
+    Gift
+  },
+  props:{
+    isSelect:  Boolean,
+    hasGift:  Boolean,
+    isOpen:  Boolean,
+    doorNumber: Number,
+  }
+}
+</script>
+
+<style>
+:root {
+  --door-border: 5px solid brown;
+  --selected-border: 5px solid yellow;
+}
+
+.door-area {
+  position: relative;
+  cursor:pointer;
+  width: 200px;
+  height: 310px;
+  border-bottom: 10px solid #AAA;
+  margin-bottom: 20px;
+  font-size: 3rem;
+  margin: 32px;
+
+  display: flex;
+  justify-content: center;
+}
+
+.door-frame {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+
+    border-left: var(--door-border);
+    border-top: var(--door-border);
+    border-right: var(--door-border);
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.door {
+  position: absolute;
+  top: 5px;
+  height: 280px;
+  width: 170px;
+  background-color: chocolate;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px;
+  z-index: 2;
+}
+
+.door-frame.selected {
+  border-left: var(--selected-border);
+  border-top: var(--selected-border);
+  border-right: var(--selected-border);
+}
+.door-number{
+  color:white;
+}
+
+.door-handle{
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  background-color: rgb(165, 42, 42);
+  margin-top: 100px;
+  margin-right: 150px;
+}
+
+.selected {
+  background-color: yellow;
+}
+.selected-number {
+  color: yellow;
+}
+
+</style>
